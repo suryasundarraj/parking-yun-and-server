66315476625324528287af2959a6823a049c90aa
suryasundarraj <suryasundarraj@live.com>
2015-12-01 13:29:20 +0530

    new files added

--- app-test/app.py
+++ app-test/app.py
@@ -1,30 +1,38 @@
+'''*********************************************************************************
+APP - SMART PARKING LOT SYSTEM
+*********************************************************************************'''
 from pubnub import Pubnub
-import datetime
-import threading
-import multiprocessing as mp
+from threading import Thread
 import sys
 
-userData = dict()
-myCar = dict()
-pubnub = Pubnub(publish_key="pub-c-a1f796fb-1508-4c7e-9a28-9645035eee90", subscribe_key="sub-c-d4dd77a4-1e13-11e5-9dcf-0619f8945a4f")
+pub_key = "pub-c-a1f796fb-1508-4c7e-9a28-9645035eee90"
+sub_key = "sub-c-d4dd77a4-1e13-11e5-9dcf-0619f8945a4f"
+
+pubnub = Pubnub(publish_key=pub_key, 
+				subscribe_key=sub_key)
+g_userData = dict()
+g_myCar = dict()
+g_lotNumber = sys.argv[1]
+g_carNumber = sys.argv[2]
 
 def callback(message, channel):
-	userData.update(message)
+	g_userData.update(message)
 
 def caRcallback(message, channel):
-	myCar.update(message)
+	g_myCar.update(message)
 
 def dataHandling(stdin):
 		l_action = int(stdin.readline().strip())
 		if(l_action == 1):
-			pubnub.publish(channel='parkingapp-req', message={"requester":"APP","deviceID":0,"requestType":1,"requestValue":0})
+			pubnub.publish(channel='parkingapp-req',message={"requester":"APP",
+				"lotNumber":0,"requestType":1,"requestValue":0})
 		elif(l_action == 2):
-			pubnub.publish(channel='parkingapp-req', message={"requester":"APP","deviceID":"001","requestType":2,"requestValue":"KA01M5512"})
+			pubnub.publish(channel='parkingapp-req', 
+					message={"requester":"APP","lotNumber":g_lotNumber,
+					"requestType":2,"requestValue":g_carNumber})
 		elif(l_action == 3):
-			pubnub.publish(channel='parkingapp-req', message={"requester":"APP","deviceID":"001","requestType":3,"requestValue":"KA01M5512"})
-		elif(l_action == 4):
-			print "\n\n", userData
-			print "\n\n", myCar
+			print "\n\n", g_userData
+			print "\n\n", g_myCar
 			
 def error(message):
     print("ERROR : " + str(message))
@@ -40,12 +48,16 @@ def disconnect(message):
 
 pubnub.subscribe(channels='parkingapp-resp', callback=callback, error=callback,
                  connect=connect, reconnect=reconnect, disconnect=disconnect)
-pubnub.subscribe(channels='KA01M5512', callback=caRcallback, error=caRcallback,
+pubnub.subscribe(channels=g_carNumber, callback=caRcallback, error=caRcallback,
                  connect=connect, reconnect=reconnect, disconnect=disconnect)
 
 if __name__ == '__main__':
 	while True:
-		t1 = threading.Thread(target=dataHandling, args=(sys.stdin,))
+		t1 = Thread(target=dataHandling, args=(sys.stdin,))
 		t1.start()
 		t1.join()
+
+		
+#End of the Script 
+##*****************************************************************************************************##
 	
--- server/server.py
+++ server/server.py
@@ -1,95 +1,153 @@
+'''*********************************************************************************
+SERVER - SMART PARKING LOT SYSTEM
+*********************************************************************************'''
+#Import the Modules Required
 from pubnub import Pubnub
 import json
 import datetime
-parkDAta = dict()
-carPark = dict()
-carMeter = dict()
-pubnub = Pubnub(publish_key="pub-c-a1f796fb-1508-4c7e-9a28-9645035eee90", subscribe_key="sub-c-d4dd77a4-1e13-11e5-9dcf-0619f8945a4f")
+from threading import Thread
+import time
 
-def checking (requester,reqtype,deviceid,requestval):
-	l_carNum = requestval
-	if (requester == "APP"):
-		if (reqtype == 1):
-			pubnub.publish(channel='parkingapp-resp', message=parkDAta)
-			print "Sent OK"
-		elif (reqtype == 2):
-			carMeter[l_carNum] = [deviceid,0,0,0]
-			if(carMeter.has_key(l_carNum)):
+pub_key = "pub-c-a1f796fb-1508-4c7e-9a28-9645035eee90"
+sub_key = "sub-c-d4dd77a4-1e13-11e5-9dcf-0619f8945a4f"
+
+#Pubnub Key Initialization 
+pubnub = Pubnub(publish_key=pub_key, 
+				subscribe_key=sub_key)
+# Holds the Present Status of all the Parking Lots Updated
+g_orginalStatus = dict()
+g_parkingStatus = dict()			
+# Notifies the App about Session Start and End
+g_sessionStatus = dict()
+# Starts the Reservation and Meterting of the Particular Slot
+g_smartMeter = dict()
+# Handles the Reservation Slot 
+g_lotReserved = dict()
+g_lotNumberList = list()
+
+def checkList(p_lotNumber):
+	l_count = 0
+	for i in range(len(g_lotNumberList)):
+		if (p_lotNumber == g_lotNumberList[i]):
+			l_count+=1
+	return l_count
+
+def closeReservation():
+	l_endTime = datetime.datetime.now()
+	time.sleep(1)
+	print len(g_lotNumberList)
+	if(len(g_lotNumberList) > 0):
+		for i in range(len(g_lotNumberList)):
+			if (g_orginalStatus.has_key(g_lotNumberList[i]) and g_orginalStatus[g_lotNumberList[i]] != 1 and len(g_lotNumberList) > 0):
+				l_totalTime = l_endTime - g_lotReserved[g_lotNumberList[i]]
+				l_totalMin = divmod(l_totalTime.days * 86400 + l_totalTime.seconds, 60)[0]
+				if(l_totalMin >= 1):
+					sessionEnd(g_lotNumberList[i],g_orginalStatus[g_lotNumberList[i]])
+					g_parkingStatus[g_lotNumberList[i]] = g_orginalStatus[g_lotNumberList[i]]
+					pubnub.publish(channel='parkingapp-resp', message={g_lotNumberList[i]:g_orginalStatus[g_lotNumberList[i]]})
+					del g_lotReserved[g_lotNumberList[i]]
+					del g_lotNumberList[i]
+					break
+			elif(g_orginalStatus.has_key(g_lotNumberList[i]) and g_orginalStatus[g_lotNumberList[i]] == 1):
+				del g_lotReserved[g_lotNumberList[i]]
+				del g_lotNumberList[i]
+				break 
+	else:
+		pass
+
+def sessionEnd(p_deviceid,p_status):
+	if(g_smartMeter.has_key(p_deviceid) and p_status == 0):
+		l_endTime = datetime.datetime.now()
+		g_smartMeter[p_deviceid][2] = l_endTime
+		l_etimeStr = str(l_endTime.hour) + ":" + str(l_endTime.minute) + ":" + str(l_endTime.second)
+		l_parsedEndTime = datetime.datetime.strptime(l_etimeStr,'%H:%M:%S').strftime('%H:%M:%S')
+		g_sessionStatus["sessionType"] = 1
+		g_sessionStatus["carNum"] = g_smartMeter[p_deviceid][0]
+		g_sessionStatus["lotNumber"] = p_deviceid
+		g_sessionStatus["endTime"] = l_parsedEndTime
+		totalTime = g_smartMeter[p_deviceid][2] - g_smartMeter[p_deviceid][1]
+		l_totalMin = divmod(totalTime.days * 86400 + totalTime.seconds, 60)[0]
+		g_sessionStatus["totalTime"] = l_totalMin
+		if(l_totalMin < 60):
+			g_sessionStatus["totalAmt"] = 20
+		elif(l_totalMin > 60 and l_totalMin < 120):
+			g_sessionStatus["totalAmt"] = 40
+		elif(l_totalMin > 120 and l_totalMin < 180):
+			g_sessionStatus["totalAmt"] = 60
+		elif(l_totalMin > 180 and l_totalMin < 240):
+			g_sessionStatus["totalAmt"] = 80
+		elif(l_totalMin > 240 and l_totalMin < 300):
+			g_sessionStatus["totalAmt"] = 100
+		elif(l_totalMin > 300 and l_totalMin < 360):
+			g_sessionStatus["totalAmt"] = 120
+		elif(l_totalMin > 360 and l_totalMin < 420):
+			g_sessionStatus["totalAmt"] = 140
+		elif(l_totalMin > 420 and l_totalMin < 480):
+			g_sessionStatus["totalAmt"] = 160
+		elif(l_totalMin > 480 and l_totalMin < 540):
+			g_sessionStatus["totalAmt"] = 180
+		elif(l_totalMin > 540 and l_totalMin < 600):
+			g_sessionStatus["totalAmt"] = 200
+		elif(l_totalMin > 600 and l_totalMin < 660):
+			g_sessionStatus["totalAmt"] = 220
+		elif(l_totalMin > 660 and l_totalMin < 720):
+			g_sessionStatus["totalAmt"] = 240
+		print pubnub.publish(channel=g_smartMeter[p_deviceid][0], message=g_sessionStatus)
+		del g_smartMeter[p_deviceid]
+	else:
+		pass
+
+def carReserved(p_lotNumber,p_status):
+	g_orginalStatus[p_lotNumber] = p_status
+	if(checkList(p_lotNumber) != 0 and p_status == 0):
+		g_parkingStatus[p_lotNumber] = 1
+	else:
+		sessionEnd(p_lotNumber,p_status)
+		g_parkingStatus[p_lotNumber] = p_status
+		pubnub.publish(channel='parkingapp-resp', message={p_lotNumber:p_status})
+
+def appRequest(p_requester,p_reqtype,p_deviceid,p_carNum):
+	if (p_requester == "APP"):
+		if (p_reqtype == 1):
+			pubnub.publish(channel='parkingapp-resp', message=g_parkingStatus)
+		elif (p_reqtype == 2):
+			g_smartMeter[p_deviceid] = [p_carNum,0,0,0]
+			if(g_smartMeter.has_key(p_deviceid)):
 				l_startTime = datetime.datetime.now()
 				l_dateStr = str(l_startTime.day) + "." + str(l_startTime.month) + "." + str(l_startTime.year)
 				l_stimeStr = str(l_startTime.hour) + ":" + str(l_startTime.minute) + ":" + str(l_startTime.second)
 				l_parsedDate = datetime.datetime.strptime(l_dateStr,'%d.%m.%Y').strftime('%m-%d-%Y')
 				l_parsedStartTime = datetime.datetime.strptime(l_stimeStr,'%H:%M:%S').strftime('%H:%M:%S')
-				carMeter[l_carNum][1] = l_startTime
-				carPark["sessionType"] = 0
-				carPark["carNum"] = l_carNum
-				carPark["lotNumber"] = deviceid
-				carPark["parkingDate"] = l_parsedDate
-				carPark["startTime"] = l_parsedStartTime
-				carPark["endTime"] = 0
-				carPark["totalTime"] = 0
-				carPark["totalAmt"] = 0
-				pubnub.publish(channel=l_carNum, message=carPark)
-				parkDAta[deviceid] = 1
-				pubnub.publish(channel='parkingapp-resp', message=parkDAta)
+				g_smartMeter[p_deviceid][1] = l_startTime
+				g_sessionStatus["sessionType"] = 0
+				g_sessionStatus["carNum"] = p_carNum
+				g_sessionStatus["lotNumber"] = p_deviceid
+				g_sessionStatus["parkingDate"] = l_parsedDate
+				g_sessionStatus["startTime"] = l_parsedStartTime
+				g_sessionStatus["endTime"] = 0
+				g_sessionStatus["totalTime"] = 0
+				g_sessionStatus["totalAmt"] = 0
+				pubnub.publish(channel=p_carNum, message=g_sessionStatus)
+				g_parkingStatus[p_deviceid] = 1
+				g_lotReserved[p_deviceid] = l_startTime 
+				if(checkList(p_deviceid)==0):
+					g_lotNumberList.append(p_deviceid)
+				pubnub.publish(channel='parkingapp-resp', message=g_parkingStatus)
 			else:
 				print "CAR NOT PARKED SUCCESSFULLY"
 
-		elif (reqtype == 3):
-			if(carMeter.has_key(l_carNum)):
-				l_endTime = datetime.datetime.now()
-				carMeter[l_carNum][2] = l_endTime
-				l_etimeStr = str(l_endTime.hour) + ":" + str(l_endTime.minute) + ":" + str(l_endTime.second)
-				l_parsedEndTime = datetime.datetime.strptime(l_etimeStr,'%H:%M:%S').strftime('%H:%M:%S')
-				carPark["sessionType"] = 1
-				carPark["carNum"] = l_carNum
-				carPark["lotNumber"] = deviceid
-				carPark["endTime"] = l_parsedEndTime
-				totalTime = carMeter[l_carNum][2] - carMeter[l_carNum][1]
-				l_totalMin = divmod(totalTime.days * 86400 + totalTime.seconds, 60)[0]
-				carPark["totalTime"] = l_totalMin
-				if(l_totalMin < 60):
-					carPark["totalAmt"] = 20
-				elif(l_totalMin > 60 and l_totalMin < 120):
-					carPark["totalAmt"] = 40
-				elif(l_totalMin > 120 and l_totalMin < 180):
-					carPark["totalAmt"] = 60
-				elif(l_totalMin > 180 and l_totalMin < 240):
-					carPark["totalAmt"] = 80
-				elif(l_totalMin > 240 and l_totalMin < 300):
-					carPark["totalAmt"] = 100
-				elif(l_totalMin > 300 and l_totalMin < 360):
-					carPark["totalAmt"] = 120
-				elif(l_totalMin > 360 and l_totalMin < 420):
-					carPark["totalAmt"] = 140
-				elif(l_totalMin > 420 and l_totalMin < 480):
-					carPark["totalAmt"] = 160
-				elif(l_totalMin > 480 and l_totalMin < 540):
-					carPark["totalAmt"] = 180
-				elif(l_totalMin > 540 and l_totalMin < 600):
-					carPark["totalAmt"] = 200
-				elif(l_totalMin > 600 and l_totalMin < 660):
-					carPark["totalAmt"] = 220
-				elif(l_totalMin > 660 and l_totalMin < 720):
-					carPark["totalAmt"] = 240
-				print pubnub.publish(channel=l_carNum, message=carPark)
-				del carMeter[l_carNum]
-			else:
-				print "ERROR CAR NUMBER"
-	
 def callback(message, channel):
-	parkDAta.update(message)
-	pubnub.publish(channel='parkingapp-resp', message=message)
-	print parkDAta
-
+	if(message.has_key("deviceID") and message.has_key("value")):
+		carReserved(message["deviceID"],message["value"])
+	else:
+		pass
+	
 def appcallback(message, channel):
-	print message
-	requester = message["requester"]
-	deviceid = message["deviceID"]
-	reqtype = message["requestType"]
-	requestval = message["requestValue"]
-	checking(requester,reqtype,deviceid,requestval)
-         
+	if(message.has_key("requester") and message.has_key("lotNumber") and message.has_key("requestType") and message.has_key("requestValue")):
+		appRequest(message["requester"],message["requestType"],message["lotNumber"],message["requestValue"])
+	else:
+		pass
+
 def error(message):
     print("ERROR : " + str(message))
    
@@ -104,3 +162,13 @@ pubnub.subscribe(channels='parkingdevice-resp', callback=callback, error=callbac
 
 pubnub.subscribe(channels='parkingapp-req', callback=appcallback, error=appcallback,
                  reconnect=reconnect, disconnect=disconnect)
+
+if __name__ == '__main__':
+	while True:
+		l_timeout = Thread(target=closeReservation)
+		l_timeout.start()
+		l_timeout.join()
+
+#End of the Script 
+##*****************************************************************************************************##
+
